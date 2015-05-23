e6df20856e8d30e87fe4a0fb1c7c34753b797f29
iev <iev@land.ru>
2015-05-23 19:55:24 +0300

    asyncio for control dlgs

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 
 from . import gui, srv
 from util.columns import *
-from util.misc import app_devtypes
+from util.misc import app_devtypes, app_devdata
 
-devdata = lambda: get_devdata('FIO', get_columns([c_ip_addr]), app_devtypes(gui))
+devdata = lambda: app_devdata('FIO', get_columns([c_ip_addr]), app_devtypes(gui))
 
